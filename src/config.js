3abe9fed4452c42aafd79b6e0c187daa73e2e282
loxx3450 <egorka231231@gmail.com>
2024-11-01 19:38:54 +0100

    4. new logic of rerender + refactoring of InfiniteDotBoard.jsx

--- src/config.js
+++ src/config.js
@@ -3,10 +3,13 @@ const CONFIG = {
     worldWidth: 200000,
     worldHeight: 200000,
     backgroundDotsColor: 0xC4C4C4,
-    dotSizeRatio: 0.16,
+    dotSizeRatio: 0.1,
     viewportMaxScale: 4,
     viewportMinScale: 0.2,
-    dotsInChunkCount: 60
+    dotsInChunkCount: 60,
+    texturePoolRatio: 0.05,
+    maximalDotsCount: 96,
+    minimalDotsCount: 40
 };
 
 export default CONFIG;
